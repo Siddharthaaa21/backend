b0e0c269da8ed2adafdb46a3cf35011a5275f131
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2024-11-20 14:56:22 +0530

    initial tut

--- node/backend-tutorial-4/backend/src/app.js
+++ node/backend-tutorial-4/backend/src/app.js
@@ -1,9 +1,41 @@
    import express from 'express';
    import mongoose from 'mongoose';
+   import cors from 'cors';
+   import cookieParser from 'cookie-parser';
 
     import { MONGO_URI } from './constants';
 
     const app= express();
+
+    app.use(cors({
+        origin:process.env.CORS_ORIGIN,
+        credentials:true
+
+    }));
+    //since the data could be shared in different formats, we need to parse the data
+    // like JSON, text, urlencoded, etc.
+    // so therefore we need to use the following middleware
+
+    app.use(express.json({
+        limit:'50mb'
+
+    }));
+    //to take data
+    //express.urlencoded is for parsing the data in the form of arrays and objects
+    app.use(express.urlencoded({
+        extended:true
+        //to take data in the form of arrays and objects
+    }));
+
+    app.use(express.static('public'));
+    //this is for static files like images pdf etc
+
+    //cookies are to take the data in the form of cookies
+    app.use(cookieParser());
+    
+    //
+    //  origin: 'http://localhost:3000',
+    // app.use(express.json());
 
 export {app};
 // Compare this snippet from node/backend-tutorial-4/backend/src/constants.js:    
--- node/backend-tutorial-4/backend/src/index.js
+++ node/backend-tutorial-4/backend/src/index.js
@@ -1,4 +1,14 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../constants";
  
-  
+connectDB()
+.then(()=>{
+    app.listen(process.env.POR||8000,()=>{  
+        console.log(`Server is running on port ${process.env.PORT||8000}`);
+    });   
+    }
+)
+.catch((err)=>{
+    console.log(err);
+
+})
